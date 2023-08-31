af1660a8c207d065d47edee29b52d4004458a82f
Ed Geraghty <edg@privacyinternational.org>
2023-08-31 11:38:38 +0100

    Remove IP refresh mechanism

--- src/docker_firefox.py
+++ src/docker_firefox.py
@@ -6,23 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
-def get_new_ip(firefox_options: webdriver.FirefoxOptions, old_driver: WebDriver = None) -> WebDriver:
-    if old_driver:
-        old_driver.quit()
-    client = docker.from_env()
-    container = client.containers.get("firefox")
-    container.restart()
-    # Wait for our container to start
-    time.sleep(60)
-    driver = webdriver.Remote(
-        command_executor="http://127.0.0.1:4444/wd/hub",
-        desired_capabilities=firefox_options.to_capabilities(),
-    )
-
-    logging.info(f"Current IP: {get_current_ip(driver)}")
-    return driver
-
-
 def get_current_ip(driver: WebDriver) -> str:
     """Get the browser's current ip by visiting myip.com"""
     driver.get("https://myip.com")
